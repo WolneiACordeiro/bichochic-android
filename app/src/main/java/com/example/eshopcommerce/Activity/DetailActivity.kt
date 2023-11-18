c4291a07081082a87ac6a5eb5bb86e044a7b3ef8
Wolnei Cordeiro <=>
2023-11-18 17:26:09 -0300

    Detail Activity UI

--- app/src/main/java/com/example/eshopcommerce/Activity/DetailActivity.kt
+++ app/src/main/java/com/example/eshopcommerce/Activity/DetailActivity.kt
@@ -1,11 +1,12 @@
-package com.example.eshopcommerce
+package com.example.eshopcommerce.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eshopcommerce.R
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
     }
 }
